a0bf3d4d00b548d2bf076b0c7ad015ae9fce676b
chris <chris.schoony@gmail.com>
2024-08-08 00:16:47 -0700

    configure collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { z, defineCollection } from "astro:content";
 
-const postCollection = defineCollection({
+const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
@@ -9,5 +9,5 @@ const postCollection = defineCollection({
 });
 
 export const collections = {
-  post: postCollection,
+  posts: postsCollection,
 };
